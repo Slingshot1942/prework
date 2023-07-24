735d15ecd3631089c4dd653cf6940b0125484652
Slingshot1942 <140355325+Slingshot1942@users.noreply.github.com>
2023-07-24 01:22:17 -0500

    Final Commit
    Final Edit

--- js/myapp.js
+++ js/myapp.js
@@ -17,11 +17,39 @@ const madlibdisplay = document.querySelector(".madlib");
 const form = document.querySelector("form");
 
 //variable for the mad lib sotry
-const story= `Adjective 1: <b>${adj1}</b> | Adjective 2: <b>${adj2}</b> | Clothing: <b>${clothing}</b> | Noun: <b>${noun}</b> | Body Part: <b>${bodypart}</b> | Food: <b>${food}</b> | Verb: <b>${verb}</b>  `
+const story= `Adjective 1: <b>${adj1}</b> | Adjective 2: <b>${adj2}</b> | Clothing: <b>${clothing}</b> | Noun: <b>${noun}</b> | Body Part: <b>${bodypart}</b> | Verb: <b>${verb}</b> | Food: <b>${food}</b>  `
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
 
+    if (adj1.value=="") {
+        alert(`Please enter a value for Adjective 1 : ${adj1.name}`)
+        adj1.focus();
+    } else if (noun.value=="") {
+        alert(`Please enter a value for Noun : ${noun.name}`)
+        noun.focus();
+    } else if (adj2.value=="") {
+        alert(`Please enter a value for Adjective 2 : ${adj2.name}`)
+        adj2.focus();
+    } else if (clothing.value=="") {
+        alert(`Please enter a value for Clothing : ${clothing.name}`)
+        clothing.focus();
+    } else if (bodypart.value=="") {
+        alert(`Please enter a value for Body Part : ${bodypart.name}`)
+        bodypart.focus();
+    } else if (verb.value=="") {
+        alert(`Please enter a value for Verb : ${verb.name}`)
+        verb.focus();
+    } else if (food.value=="") {
+        alert(`Please enter a value for Food : ${food.name}`)
+        food.focus();
+    } else {
+        madLibstoryDisplay();
+    }
+
+});
+
+function madLibstoryDisplay() {
     madlibdisplay.innerHTML = story
 
     .replace(`${adj1}`, adj1.value)
@@ -31,4 +59,4 @@ form.addEventListener("submit", function (event) {
     .replace(`${bodypart}`, bodypart.value)
     .replace(`${verb}`, verb.value)
     .replace(`${food}`, food.value)
-});
+}
